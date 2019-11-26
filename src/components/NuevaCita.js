99c887e646aa87ac3f376eab46cea0a9cf3de645
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-11-26 05:04:03 -0600

    showing error message

--- src/components/NuevaCita.js
+++ src/components/NuevaCita.js
@@ -45,12 +45,19 @@ class NuevaCita extends Component {
   }
 
   render() { 
+
+    // extraer valos del state
+    const { error } = this.state;
+
+
     return ( 
       <div className="card mt-5 py-5">
         <div className="card-body">
           <h2 className="card-title text-center mb-5">
             Llena el formulario para crear una nueva cita
           </h2>
+
+          { error ? <div className="alert alert-danger mt-2 mb5 text-center"> Todos los campos son obligatorios</div> : null }
 
           <form onSubmit={this.handleSubmit}>
             <div className="form-group row">
